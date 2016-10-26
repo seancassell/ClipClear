7e287ed81df1671a19f0e562fe7b47ece031105c
Sean Cassell <sean.cassell@outlook.com>
2016-10-26 19:44:46 -0400

    CHANGELOG FOR v. 3.1.1:
    -> Incremented version number to 3.1.1 [build.gradle];
    -> New colour scheme [styles.xml];
    -> Significantly larger colour palette (Material Design colours) [colors.xml];
    -> Slightly shorter length of vibration after clicking btnErase [MainActivity.java];
    -> Changed text of lblInstructions [activity_main.xml];
    -> Added licence file [LICENSE.txt];
    -> Updated copyright notices (multiple files).

--- app/src/main/java/net/seancassell/clipclear/MainActivity.java
+++ app/src/main/java/net/seancassell/clipclear/MainActivity.java
@@ -1,5 +1,21 @@
 /*******************************************************************************
- * Copyright © 2016 Sean Cassell. Licenced under GPLv3.
+ *     ClipCear - Exceedingly simple clipboard management app.
+ *     Copyright © 2016 Sean Cassell.
+ *
+ *     Developer email: sean.cassell@outlook.com
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
 package net.seancassell.clipclear;
@@ -23,7 +39,7 @@ public class MainActivity extends Activity {
 
 	public void handleBtnEraseClick (View view) {
 		Vibrator HAPTIC_SIM = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-		HAPTIC_SIM.vibrate(100);
+		HAPTIC_SIM.vibrate(75);
 		ClipboardManager clipMgr = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
 		clipMgr.setPrimaryClip(ClipData.newPlainText("", ""));
 		Toast.makeText(getApplicationContext(), "The clipboard has been successfully erased.", Toast.LENGTH_LONG).show();
